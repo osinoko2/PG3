ba7b5072d529ad1cab913ce8f2e3aa95999dafb0
osinoko <k023g0114@g.neec.ac.jp>
2024-10-08 12:07:45 +0900

    再帰関数

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int Recursive(int time, int money){
+	if(time <= 1){
+		return (100);
+	}
 
-	printf("ようこそ!!");
+	money = money * 2 - 50;
+
+	return (money + Recursive(time - 1, money));
+}
+
+int main() {
+	int time = 8;
+	int money = 100;
+	int salary = 1072;
+	int result;
+	int result2;
+
+	result = salary * time;
+	result2 = Recursive(time, money);
+
+	printf("一般的な賃金体系で%d時間 = %d\n", time, result);
+	printf("再帰的な賃金体系で%d時間 = %d", time, result2);
 
 	return 0;
 }
