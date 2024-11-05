c2cf3e9a223ccf3920ae0aaeb9652534cb3b410a
osinoko <k023g0114@g.neec.ac.jp>
2024-11-05 21:39:38 +0900

    クラステンプレート

--- main.cpp
+++ main.cpp
@@ -65,12 +65,65 @@ void Enemy::Update() {
 	(this->*sphaseTable[static_cast<size_t>(phase_)])();
 }
 
+template <typename Type1, typename Type2>
+
+class Calc {
+public:
+	static void Min(Type1 a, Type2 b);
+};
+
+template <typename Type1, typename Type2>
+void Calc<Type1, Type2>::Min(Type1 a, Type2 b) {
+
+	if (a > b) {
+
+		if (typeid(b) == typeid(int)) {
+			printf("%d\n", b);
+
+			return;
+		} else if (typeid(b) == typeid(float)) {
+			printf("%.02f\n", b);
+
+			return;
+		} else {
+			printf("%.02lf\n", b);
+
+			return;
+		}
+	} else if (a < b) {
+
+		if (typeid(a) == typeid(int)) {
+			printf("%d\n", a);
+
+			return;
+		} else if (typeid(a) == typeid(float)) {
+			printf("%.02f\n", a);
+
+			return;
+		} else {
+			printf("%.02lf\n", a);
+
+			return;
+		}
+	} else {
+
+		printf("同じ値です\n");
+	}
+}
+
 Enemy enemy;
 
 int main(int argc, const char *argv[]) {
-	enemy.Update();
-	enemy.Update();
-	enemy.Update();
+	int a = 5;
+	int b = 25;
+	float c = 12.4f;
+	float d = 12.3f;
+	double e = 255.9;
+	double f = 256.2;
+
+	Calc<int, int>::Min(a, b);
+	Calc<float, float>::Min(c, d);
+	Calc<double, double>::Min(e, f);
 
 	return 0;
 }
